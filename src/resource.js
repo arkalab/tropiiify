52c084d88a9396739a9cd74dd78ec84fbc9f2c8e
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2023-09-29 18:43:05 -0300

    Fix URL bugs, move options to prop

--- src/resource.js
+++ src/resource.js
@@ -16,15 +16,15 @@ class Resource {
   constructor(data = {}, map, options = {}) {
     //this.data = data
     //this.map = map
-    Object.assign(this, options)
+    this.options = options
 
     for (let property in map) {
       this[property] = this.extractValue(data, map[property]) //Must/should have an 'id' prop
     }
 
     const cleanId = Resource.sanitizeString(this.id)
-    this.path = path.join(this.output, cleanId) //manifest filesystem path
-    this.baseId = new URL(`/iiif/${cleanId}`, this.baseId)  //manifest URI
+    this.path = path.join(this.options.output, cleanId) //manifest filesystem path
+    this.baseId = new URL(`/iiif/${cleanId}`, this.options.baseId).toString()  //manifest URI
 
     this.photo = this.extractValue(data, tropy('photo')).map((photo) => ({
       checksum: this.extractValue(photo, tropy('checksum')),
@@ -47,6 +47,34 @@ class Resource {
     return this[property].replace(LINK, '<a href="$2" target="_blank">$1</a>')
   }
 
+  async createManifest() {
+    const normalizedManifest = manifestBuilder.createManifest(
+      `${this.baseId}/manifest.json`,
+      manifest => {
+        manifest.addLabel(this.label);
+        this.summary && manifest.addSummary(this.summary)
+        this.rights && manifest.setRights(this.rights);
+        this.requiredstatementValue && manifest.setRequiredStatement({
+          label: this.options.requiredStatementLabel,
+          value: `${this.options.requiredStatementText} ${this.assembleHTML('requiredstatementValue')}`.trim() //Remove eventual leading whitespace
+        })
+        manifest.setHomepage({
+          id: this.homepageValue,
+          type: 'Text',
+          label: { "none": [this.options.homepageLabel] }, //Falls back to default
+          format: 'text/html'
+        })
+        //manifest.addSeeAlso()
+        //manifest.addThumbnail()
+        //props.latitude && props.longitude && manifest.addNavPlace(latitude, longitude)
+        manifest.addThumbnail({ id: new URL('/square/600,/0/default.jpg', this.baseId).toString(), type: 'Image', format: 'image/jpeg' });
+        this.fillMetadata(manifest) //assigns all this.metadata{{Label}} props  
+        this.createCanvases(manifest)
+      }
+    )
+    return manifestBuilder.toPresentation3({ id: normalizedManifest.id, type: 'Manifest' });
+  }
+
   fillMetadata(manifest) {
     for (let property in this) {
       if (property.startsWith('metadata') && this[property]) {
@@ -58,39 +86,12 @@ class Resource {
     }
   }
 
-  async createManifest() {
-    const normalizedManifest = manifestBuilder.createManifest(
-      new URL ('/manifest.json', this.baseId).toString(),
-      manifest => {
-        manifest.addLabel(this.label);
-        this.summary && manifest.addSummary(this.summary)
-        this.rights && manifest.setRights(this.rights);
-        this.requiredstatementValue && manifest.setRequiredStatement({
-          label: this.requiredStatementLabel,
-          value: `${this.requiredStatementText} ${this.assembleHTML('requiredstatementValue')}`.trim() //Remove eventual leading whitespace
-        })
-        manifest.setHomepage({
-          id: this.homepageValue,
-          type: 'Text',
-          label: { "none": [this.homepageLabel] }, //Falls back to default
-          format: 'text/html'
-        })
-        //manifest.addSeeAlso()
-        //manifest.addThumbnail()
-        //props.latitude && props.longitude && manifest.addNavPlace(latitude, longitude)
-        this.fillMetadata(manifest) //assigns all this.metadata{{Label}} props  
-        this.createCanvases(manifest)
-      }
-    )
-    return manifestBuilder.toPresentation3({ id: normalizedManifest.id, type: 'Manifest' });
-  }
-
   createCanvases(manifest) {
     for (let [index, photo] of this.photo.entries()) {
-      const canvasId = new URL (`/canvas/${index}`, this.baseId).toString()
-      const annPageId = new URL (`/annotation-page/${index}`, canvasId).toString()
-      const annId = new URL (`/annotation/${index}`, canvasId).toString()
-      const bodyId = new URL (`/${photo.checksum}/${photo.checksum}${path.extname(photo.path)}`, this.baseId).toString()
+      const canvasId = `${this.baseId}/canvas/${index}`
+      const annPageId = `${canvasId}/annotation-page/${index}`
+      const annId = `${this.baseId}/annotation/${index}`
+      const bodyId = `${this.baseId}/${photo.checksum}/full/max/0/default${path.extname(photo.path)}`
       manifest.createCanvas(canvasId, (canvas) => {
         canvas.width = photo.width;
         canvas.height = photo.height;
@@ -104,7 +105,14 @@ class Resource {
               type: 'Image',
               format: photo.mimetype,
               width: photo.width,
-              height: photo.height
+              height: photo.height,
+              service: [
+                {
+                  "id": `${this.baseId}/${photo.checksum}`,
+                  "type": "ImageService3",
+                  "profile": "http://iiif.io/api/image/3/level0.json"
+                }
+              ]
             }
           }
         )
