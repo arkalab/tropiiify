90f8d945af7161ce2240426a52880fa5ceb00f85
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-01-31 18:17:53 -0300

    Add missing operator

--- src/resource.js
+++ src/resource.js
@@ -33,7 +33,7 @@ class Resource {
       mimetype: this.extractValue(photo, tropy('mimetype')),
       path: this.extractValue(photo, tropy('path')),
       note: this.extractValue(this.extractValue(photo, tropy('note'))?.[0], tropy('html')),
-      selection: this.extractValue(photo, tropy('selection')).map((selection) => ({
+      selection: this.extractValue(photo, tropy('selection'))?.map((selection) => ({
         note: this.extractValue(this.extractValue(selection, tropy('note'))?.[0], tropy('html')),
         x: this.extractValue(selection, tropy('x')),
         y: this.extractValue(selection, tropy('y')),
